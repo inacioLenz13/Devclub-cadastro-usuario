699b1724e1b6332ce18674e59022f503db00732f
inacioLenz13 <144399978+inacioLenz13@users.noreply.github.com>
2025-08-15 16:10:52 -0300

    integrating the backend with the frontend

--- devclub-cadastro-usuarios/src/pages/Home/index.jsx
+++ devclub-cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,26 +1,38 @@
 import "./style.css";
+import Lixeira from "../../assets/lixeira.png";
 
 function Home() {
+  const users = [
+    {
+      id: "123234234",
+      name: "João",
+      age: 25,
+      email: "inaciolenz@gmail.com",
+    },
+  ];
+
   return (
     <div className="container">
       <form>
         <h1>Cadastro de Usuarios</h1>
-        <input name="nome" type="text" />
-        <input name="idade" type="number" />
-        <input name="imail" type="email" />
+        <input placeholder="Nome" name="nome" type="text" />
+        <input placeholder="Idade" name="idade" type="number" />
+        <input placeholder="E-email" name="imail" type="email" />
         <button type="button">Cadastrar</button>
       </form>
 
-      <div>
-        <div>
-          <p>Nome</p>
-          <p>Idade</p>
-          <p>Email</p>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome:<span>{user.name}</span></p>
+            <p>Idade:<span>{user.age}</span></p>
+            <p>Email:<span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Lixeira} className="lixeira-icon" />
+          </button>
         </div>
-        <button>
-          <img />
-        </button>
-      </div>
+      ))}
     </div>
   );
 }
